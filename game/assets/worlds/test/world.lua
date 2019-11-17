8180e35583cd87ec76e371b5af41fe0164e46a81
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-11-17 11:39:29 -0300

    use lua instead of tsv for describing levels

---
+++ game/assets/worlds/test/world.lua
@@ -0,0 +1,58 @@
+return {
+    {
+        entity = "player",
+        x = 150,
+        y = 400,
+        w = 50,
+        h = 50,
+        color = "red"
+    },
+    {
+        entity = "enemy",
+        x = 1050,
+        y = 400,
+        w = 50,
+        h = 50,
+        color = "green"
+    },
+    {
+        entity = "enemy",
+        x = 1750,
+        y = 400,
+        w = 70,
+        h = 70,
+        color = "green"
+    },
+    {
+        entity = "brick",
+        x = 1050,
+        y = 100,
+        w = 2100,
+        h = 200,
+        color = "white"
+    },
+    {
+        entity = "brick",
+        x = 25,
+        y = 400,
+        w = 50,
+        h = 400,
+        color = "white"
+    },
+    {
+        entity = "brick",
+        x = 2075,
+        y = 400,
+        w = 50,
+        h = 400,
+        color = "white"
+    },
+    {
+        entity = "brick",
+        x = 1050,
+        y = 700,
+        w = 2100,
+        h = 200,
+        color = "white"
+    }
+}
--- game/models/level.lua
+++ game/models/level.lua
@@ -1,52 +1,10 @@
 local level_model = { }
 
-function split(inputstr, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t = { }
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-        table.insert(t, str)
-    end
-    return t
-end
-
-function load_tsv(filename)
-    local outlet = { }
-
-    local contents, size = love.filesystem.read('string', filename)
-    local lines = split(contents, "\n")
-    local headers = { }
-    for i, line in pairs(lines) do
-        local fields = split(line, "\t")
-        if i == 1 then
-            headers = fields
-        else
-            local record = { }
-            for j, header in pairs(headers) do
-                record[header] = fields[j]
-            end
-            table.insert(outlet, record)
-        end
-    end
-
-    return outlet
-end
-
-function write_tsv(records)
-    for _, record in pairs(records) do
-        print("---")
-        for key, value in pairs(record) do
-            print(key .. ": " .. value)
-        end
-    end
-end
-
 function level_model:load_level(world)
     local level_path = "assets/worlds/" .. world .. "/"
-    local world_data_path = level_path .. "world.tsv"
-    local world = load_tsv(world_data_path)
-    return world
+    local world_data_path = level_path .. "world.lua"
+    local world = love.filesystem.load(world_data_path)
+    return world()
 end
 
 return level_model
