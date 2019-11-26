a524446aaf91a505aea9ebf7e9019254881722c8
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-11-26 19:10:43 -0300

    Get pawn sprites

--- game/views/entities/pawn.lua
+++ game/views/entities/pawn.lua
@@ -1,13 +1,33 @@
+local function listAllSprites(spriteFolder)
+    local sprites = { }
+    local sourceFolder = "/assets/sprites/pawns/" .. spriteFolder
+    local folders = love.filesystem.getDirectoryItems(sourceFolder)
+
+    for _, position in pairs(folders) do
+        local positionFolder = sourceFolder .. "/" .. position .. "/"
+        local frames = love.filesystem.getDirectoryItems(positionFolder)
+        sprites[position] = { }
+        for _, frame in pairs(frames) do
+            local imagePath = positionFolder .. frame
+            local image = love.graphics.newImage(imagePath)
+            print(position)
+            table.insert(sprites[position], image)
+        end
+    end
+
+    return sprites
+end
+
 local function PawnView(spriteFolder)
     local self = { }
     self.z = 1
     -- TODO load sprite sheet
     self.drawable = false
+    self.sprites = { }
+    self.frame = 1
+    self.subFrame = 1
     if spriteFolder then
-        self.sprites = {
-            stillRight = love.graphics.newImage("/assets/sprites/pawns/" .. spriteFolder .. "/StillRight.png"),
-            -- TODO get remaining sprites
-        }
+        self.sprites = listAllSprites(spriteFolder)
         self.drawable = true
     end
 
@@ -21,13 +41,33 @@ local function PawnView(spriteFolder)
 
     function self:draw(entity)
         -- TODO get current state
-        local sprite = self.sprites.stillRight
+        -- TODO get current direction
+        -- possible states:
+        -- - still
+        -- - walking
+        -- - punching
+        -- - punched
+        local state = "Still"
+        local direction = "Right"
+        local template = state .. direction
+        local sprite = self.sprites[template][self.frame]
 
         -- TODO draw sprite according to entity's body and frame
         -- TODO correct X and Y according to sprite size
         local x = entity.body:getX() - 25
         local y = entity.body:getY() - 40
         love.graphics.draw(sprite, x, y)
+
+        -- updating frame
+        self.subFrame = self.subFrame + 1
+        if self.subFrame >= 24 then
+            if self.frame == #self.sprites[template] then
+                self.frame = 1
+            else
+                self.frame = self.frame + 1
+            end
+            self.subFrame = 1
+        end
     end
 
     return self
